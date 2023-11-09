8e2e213299bc8acc60fbcafaeab05e0c9d04f836
GuilhermeCasagrandeF <guilherme.casagrande@soeltech.com.br>
2023-11-09 15:09:52 -0300

    Utilizando o contextProvider para usuário

--- src/routes.js
+++ src/routes.js
@@ -2,20 +2,16 @@ import Login from 'pages/Login';
 import Feira from 'pages/Feira';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Carrinho from 'pages/Carrinho';
-import { useState } from 'react';
-import { UsuarioContext } from 'shared/context/Usuario';
+import { UsuarioProvider } from 'shared/context/Usuario';
 
 function Router() {
-    const [nome, setNome] = useState('');
-    const [saldo, setSaldo] = useState(0);
-
     return (
         <BrowserRouter>
             <Switch>
                 <Route exact path='/'>
-                    <UsuarioContext.Provider value={{nome, setNome, saldo, setSaldo}}>
+                    <UsuarioProvider>
                         <Login />
-                    </UsuarioContext.Provider>
+                    </UsuarioProvider>
                 </Route>
                 <Route path='/feira'>
                     <Feira />
--- src/shared/context/Usuario.js
+++ src/shared/context/Usuario.js
@@ -1,4 +1,15 @@
+import { useState } from "react";
 import { createContext } from "react";
 
 export const UsuarioContext = createContext();
 
+export const UsuarioProvider = ({children}) => { //prop padrão
+    const [nome, setNome] = useState('');
+    const [saldo, setSaldo] = useState(0);
+
+    return (
+        <UsuarioContext.Provider value={{nome, setNome, saldo, setSaldo}}>
+            {children}
+        </UsuarioContext.Provider>
+    )
+}
